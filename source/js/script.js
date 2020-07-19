03f2276dfac15962d6fa801975e803efe1a10398
IvanSabV <60038980+IvanSabV@users.noreply.github.com>
2020-07-19 21:11:10 +0400

    tablet + desktop version

--- source/js/script.js
+++ source/js/script.js
@@ -32,3 +32,26 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+
+var modalWindow = document.querySelector('.reg');
+var regBtn = document.querySelector('.head__btn');
+var closeBtn = document.querySelector('.close');
+
+regBtn.onclick = function() {
+  modalWindow.style.display = "block";
+}
+
+window.onclick = function() {
+  modalWindow.style.display = "none";
+}
+
+closeBtn.onclick = function() {
+  modalWindow.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modalWindow) {
+      modalWindow.style.display = "none";
+    }
+}
